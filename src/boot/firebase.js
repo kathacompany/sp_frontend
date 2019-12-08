d3088e49b9432e7a0bc8a8f432a5f320cbefcf0c
Shayne Meiko Abao <smabao@up.edu.ph>
2019-12-08 14:40:18 +0800

    Added firebase

---
+++ src/boot/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase/app'
+
+import 'firebase/auth'
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyA34MAQpn8VN1VvswT357d9pFI3MfPx-hA',
+  authDomain: 'jopsis-cfca3.firebaseapp.com',
+  databaseURL: 'https://jopsis-cfca3.firebaseio.com',
+  projectId: 'jopsis-cfca3',
+  storageBucket: 'jopsis-cfca3.appspot.com',
+  messagingSenderId: '820579840616',
+  appId: '1:820579840616:web:df6a8ee0713b125c6201dd',
+  measurementId: 'G-JP3KQ1NCBE'
+}
+
+let firebaseApp = firebase.initializeApp(firebaseConfig)
+let firebaseAuth = firebaseApp.auth()
+
+export { firebaseAuth }
---
+++ src/store/store-auth.js
@@ -0,0 +1,53 @@
+import { LocalStorage } from 'quasar'
+import { firebaseAuth } from 'boot/firebase'
+
+const state = {
+  loggedIn: false
+}
+
+const mutations = {
+  setLoggedIn (state, value) {
+    state.loggedIn = value
+  }
+}
+
+const actions = {
+  loginUser (context, payload) {
+    firebaseAuth.signInWithEmailAndPassword(payload.email, payload.password)
+      .then(response => {
+        console.log('response: ', response)
+      })
+      .catch(error => {
+        console.log('error.message:', error.message)
+      })
+  },
+  logoutUser () {
+    console.log('logoutUser')
+    firebaseAuth.signOut()
+  },
+  handleAuthStateChange ({ commit }) {
+    firebaseAuth.onAuthStateChanged(user => {
+      if (user) {
+        commit('setLoggedIn', true)
+        LocalStorage.set('loggedIn', true)
+        this.$router.push('/AdministrativeHomepage')
+      } else {
+        commit('setLoggedIn', false)
+        LocalStorage.set('loggedIn', false)
+        this.$router.replace('/pages/administrative/Login')
+      }
+    })
+  }
+}
+
+const getters = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
